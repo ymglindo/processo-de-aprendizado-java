3f806d09191998969132a81b2929051bd0a74f3b
ymg_x64 <107366094+ymglindo@users.noreply.github.com>
2025-10-20 20:07:31 -0300

    Create Primeiroprojeto.java

---
+++ Projetos/Primeiroprojeto.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Exercicios1 {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+
+        int x = sc1.nextInt();
+        int y = sc2.nextInt();
+
+        int soma = x+y;
+
+        System.out.println("O resultado da sua soma é igual a " + soma);
+
+    }
+}
+
+# Exercício 2 
+
+  
